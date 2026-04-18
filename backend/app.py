71bd855b38f256275220bd99c447e08c5376ad42
MotionForce <74267742+MotionForce@users.noreply.github.com>
2026-04-18 10:37:31 -0400

    basic painter

--- backend/app.py
+++ backend/app.py
@@ -6,78 +6,28 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 import dotenv
 
-from prompts import mixer_prompt
+from painter import painter_llm
+from prompts import mixer_prompt, painter_prompt
+from mixer import mixer_llm
 
 dotenv.load_dotenv()
-
-
-owned_red_paint: int = 0
-owned_green_paint: int = 0
-owned_blue_paint: int = 0
-
-
-# @tool
-# def saturate_colors(color: str, runtime: ToolRuntime[Context]) -> str:
-#     """Change the saturation of a mixed color."""
-#     return "Your red is now dark."
-
-@tool
-def go_collect_paint(color: str) -> str:
-    """Collect primary paints from resource station and add them to your inventory."""
-    global owned_red_paint, owned_green_paint, owned_blue_paint
-    if color == "red":
-        owned_red_paint += 1
-    elif color == "green":
-        owned_green_paint += 1
-    elif color == "blue":
-        owned_blue_paint += 1
-    return f"You have collected paint. You now have {owned_red_paint} red paint, {owned_green_paint} green paint, and {owned_blue_paint} blue paint."
-
-
-@tool
-def mix_paints(color_1: str, color_2: str) -> str:
-    """Mix together the paints that you already own to obtain yellow, magenta or cyan colors."""
-    global owned_red_paint, owned_green_paint, owned_blue_paint
-    if (color_1 == "red" and color_2 == "green") or (color_1 == "green" and color_2 == "red"):
-        owned_red_paint -= 1
-        owned_green_paint -= 1
-        return "You have obtained yellow!"
-    elif (color_1 == "red" and color_2 == "blue") or (color_1 == "blue" and color_2 == "red"):
-        owned_red_paint -= 1
-        owned_blue_paint -= 1
-        return "You have obtained magenta!"
-    elif (color_1 == "green" and color_2 == "blue") or (color_1 == "blue" and color_2 == "green"):
-        owned_green_paint -= 1
-        owned_blue_paint -= 1
-        return "You have obtained cyan!"
-    return ""
-
-
-mixer_llm = ChatGoogleGenerativeAI(
-    model="gemma-4-26b-a4b-it",
-    temperature=1.0,  # Gemini 3.0+ defaults to 1.0
-    max_tokens=None,
-    timeout=None,
-    max_retries=2
-).bind_tools([go_collect_paint, mix_paints])
 
 app = Flask(__name__)
 
 
 @app.route("/mixer")
 def run_mixer():
-    global owned_red_paint, owned_green_paint, owned_blue_paint
     target_colors = request.args.get("color")
     owned_colors = request.args.get("owned").split(" ")
 
-    owned_red_paint = int(owned_colors[0])
-    owned_green_paint = int(owned_colors[1])
-    owned_blue_paint = int(owned_colors[2])
+    mixer_owned_red_paint = int(owned_colors[0])
+    mixer_owned_green_paint = int(owned_colors[1])
+    mixer_owned_blue_paint = int(owned_colors[2])
 
     print(", ".join(target_colors.split(" ")))
     messages = [("system", mixer_prompt),
                 ("human",
-                 f"You need to obtain {", ".join(target_colors.split(" "))}. You have {owned_red_paint} red paint, {owned_green_paint} green paint, and {owned_blue_paint} blue paint."),
+                 f"You need to obtain {", ".join(target_colors.split(" "))}. You have {mixer_owned_red_paint} red paint, {mixer_owned_green_paint} green paint, and {mixer_owned_blue_paint} blue paint."),
                 ]
 
     result = mixer_llm.invoke(messages)
@@ -86,7 +36,38 @@ def run_mixer():
         print(result.content)
         print(result.tool_calls)
         print(result.usage_metadata)
+        print(result)
+    return jsonify(
+        result=result.tool_calls,
+    )
 
+
+@app.route("/painter")
+def run_painter():
+    template = request.args.get("template")
+    owned_colors = request.args.get("owned").split(" ")
+
+    painter_owned_red_paint = int(owned_colors[0])
+    painter_owned_green_paint = int(owned_colors[1])
+    painter_owned_blue_paint = int(owned_colors[2])
+    painter_owned_yellow_paint = int(owned_colors[3])
+    painter_owned_cyan_paint = int(owned_colors[4])
+    painter_owned_magenta_paint = int(owned_colors[5])
+
+    print(template)
+
+    messages = [("system", painter_prompt),
+                ("human",
+                 f"You need to fill in this template: {template}. You have {painter_owned_red_paint} red paint, {painter_owned_green_paint} green paint, {painter_owned_blue_paint} blue paint, {painter_owned_yellow_paint} yellow paint, {painter_owned_magenta_paint} magenta paint, and {painter_owned_cyan_paint} cyan paint."),
+                ]
+
+    result = painter_llm.invoke(messages)
+
+    if isinstance(result, AIMessage) and result.tool_calls:
+        print(result.content)
+        print(result.tool_calls)
+        print(result.usage_metadata)
+        print(result)
     return jsonify(
         result=result.tool_calls,
     )
--- backend/prompts.py
+++ backend/prompts.py
@@ -18,3 +18,20 @@ Skip obtaining colors that are either red, green and blue.
 
 Put all the required tools you would use in a single answer.
 """
+
+painter_prompt = """You are a worker in an office puts colors into specific positions on a template. The template is just a list of colors.
+There are six possible colors: red, green, blue, cyan, magenta and yellow. 
+You can collect these colors at the central resource station. Then you can put them into the right positions based on the provided template.
+
+You have access to the following tools:
+- go_collect_paint: use this to collect a specific paint color from the resource station
+- combine_paints: use this to mix together the paints that you have to obtain new colors
+
+Look at the list of colors that you already have to choose how the obtain and position the target color.
+
+Placing colors in the template consumes the target color.
+
+Cyan, magenta and yellow need time to become available and won't be ready to collect immediately. Optimize your process based on this fact to never be idling.
+
+Put all the required tools you would use in a single answer.
+"""
